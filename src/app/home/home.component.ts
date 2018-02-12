37c1598c16e580b0029ad3aad493553bd0473fbb
Roman Ivanenko <Roman.Ivanenko@emc.com>
2018-02-11 21:53:58 -0800

    added Event Binding.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,10 +10,18 @@ export class HomeComponent implements OnInit {
   itemCount: number = 4;
   btnText: string = 'Add an Item';
   goalText: string = 'My first life goal';
+  goals = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
   }
 
 }
