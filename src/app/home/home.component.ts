23c39162a0bf70a7363e4956c021392bdf0ff28f
Roman Ivanenko <Roman.Ivanenko@emc.com>
2018-02-11 22:34:53 -0800

    added Animation for remove item.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,13 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
             style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
             style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
           ]))]), {optional: true})
+        ,
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-out', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+          ]))]), {optional: true})
       ])
     ])
   ]
@@ -39,4 +46,7 @@ export class HomeComponent implements OnInit {
     this.itemCount = this.goals.length;
   }
 
+  removeItem(i) {
+    this.goals.splice(i, 1);
+  }
 }
